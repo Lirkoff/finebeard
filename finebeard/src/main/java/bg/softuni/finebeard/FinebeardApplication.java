c825e149f7db3fbb017ed6165cb4585b62175268
Lirkoff <martinlirkoff@gmail.com>
2024-07-09 11:01:31 +0300

    Initial

---
+++ finebeard/src/main/java/bg/softuni/finebeard/FinebeardApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.finebeard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinebeardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinebeardApplication.class, args);
+	}
+
+}
---
+++ finebeard/src/test/java/bg/softuni/finebeard/FinebeardApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.finebeard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinebeardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
